86ec633171cf075607f164561ecda2b902385c10
Syba789 <95074177+Syba789@users.noreply.github.com>
2022-03-04 10:36:00 +0000

    done

--- p1final.c
+++ p1final.c
@@ -8,13 +8,18 @@ void input(int *num1, int *den1, int *num2, int *den2)
 }
 void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
 {
-  *num3= (num1*den1) + (num2*den2);
-  *den3=den1*den2;
+  *num3= (num1*den2) + (num2*den1);
+  *den3=den2*den1;
 }
 void output(int num1, int den1, int num2, int den2, int num3, int den3)
 {
-  
-  printf("sum 0f %d/%d + %d/%d is %d/%d",num1,den1,num2,den2,num3,den3);
+  int gcd;
+  for(int i=1; i<=num3 && i<=den3; ++i)
+    {
+      if(num3%i==0 && den3%i==0)
+        gcd=i;
+      }
+  printf("sum 0f %d/%d + %d/%d is %d/%d",num1,den1,num2,den2,num3/gcd,den3/gcd);
 }
 int main()
 {
